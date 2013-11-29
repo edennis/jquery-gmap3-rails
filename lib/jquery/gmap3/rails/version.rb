d1058e88aa6bd65f0a3c50a9d795f7c573bd6c2c
Vincenzo Farruggia <vincenzo.farr@gmail.com>
2013-11-29 11:02:11 +0100

    Bump versions (gmap,gem)

--- lib/jquery/gmap3/rails/version.rb
+++ lib/jquery/gmap3/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Gmap3
     module Rails
-      VERSION = "0.1.1"
-      JQUERY_GMAP3_VERSION = "4.1"
+      VERSION = "0.1.2"
+      JQUERY_GMAP3_VERSION = "5.1.1"
     end
   end
 end
